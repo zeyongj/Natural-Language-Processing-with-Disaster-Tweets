5d16eea4107993bc1a2ad7c9a84b5378a8bae4e9
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-04-21 22:29:45 -0700

    Update natural_language_processing_with_disaster_tweets.py
    Load the necessary libraries and read the datasets.

--- natural_language_processing_with_disaster_tweets.py
+++ natural_language_processing_with_disaster_tweets.py
@@ -1,1 +1,12 @@
+import pandas as pd
+import numpy as np
+import re
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score
+
+# Load the datasets
+train = pd.read_csv("train.csv")
+test = pd.read_csv("test.csv")
 
