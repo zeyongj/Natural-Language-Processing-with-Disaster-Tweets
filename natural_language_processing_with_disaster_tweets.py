397ab6f3a196cea68dd808411aff7ce99409af6b
Zeyong Jin <52082640+zeyongj@users.noreply.github.com>
2023-04-21 22:30:09 -0700

    Update natural_language_processing_with_disaster_tweets.py
    Perform text preprocessing and use both "text" and "keyword" columns for training.

--- natural_language_processing_with_disaster_tweets.py
+++ natural_language_processing_with_disaster_tweets.py
@@ -10,3 +10,17 @@ from sklearn.metrics import accuracy_score, f1_score
 train = pd.read_csv("train.csv")
 test = pd.read_csv("test.csv")
 
+def preprocess_text(text):
+    text = re.sub(r"http\S+|www\S+|https\S+", "", text, flags=re.MULTILINE)
+    text = text.lower()
+    text = re.sub(r'\W', ' ', text)
+    text = re.sub(r'\s+', ' ', text).strip()
+    return text
+
+train["text"] = train["text"].apply(preprocess_text)
+test["text"] = test["text"].apply(preprocess_text)
+train["keyword"] = train["keyword"].fillna("").apply(preprocess_text)
+test["keyword"] = test["keyword"].fillna("").apply(preprocess_text)
+
+train["combined_text"] = train["keyword"] + " " + train["text"]
+test["combined_text"] = test["keyword"] + " " + test["text"]
